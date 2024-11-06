a1543710b127b9bea67424366d1fc3e8f1a55358
Nicollas Luz <nicollasgoias@hotmail.com>
2024-11-06 17:13:26 -0300

    criando home

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
 import './App.css'
-import Header from './components/Header'
-import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
-    <Header />
-    <section className='container'>
-      <div className='apresentacao'>
-        <p>
-          Olá, sou <br/>
-          <span>Rêmulo Borges</span> <br/>
-          Dev Full Stack
-        </p>
-        <button className='btn btn-red'>
-          Saiba mais sobre mim
-        </button>
-      </div>
-      <figure>
-        <img className='img-home' src="/developer-red.svg" alt="Imagem de Home" />
-      </figure>
-    </section>
-    <Footer />
+
     </>
   )
 }
