df9f3c64c32b4d6814856bbe57085219b7a2573a
Nicollas Luz <nicollasgoias@hotmail.com>
2024-11-06 17:24:33 -0300

    criando componente Container

---
+++ src/components/Container/index.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types'
+import styles from './Container.module.css'
+
+// eslint-disable-next-line react/prop-types
+function Container({ children }) {
+    return (
+        <section className={styles.container}>
+            {children}
+        </section>
+    )
+}
+
+    Container.propTypes = {
+        children: PropTypes.any
+    }.isRequired
+        
+export default Container
