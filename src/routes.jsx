17d2d6180bbb7d197646b7fbacc22a5e996cf036
Nicollas Luz <nicollasgoias@hotmail.com>
2024-11-06 18:12:14 -0300

    editando rotas e import da pagina base

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,24 +4,20 @@ import Sobre from './pages/Sobre'
 import Projetos from './pages/Projetos'
 import Contatos from './pages/Contatos'
 import Page404 from './pages/Page404'
-import Header from './components/Header'
-import Container from './components/Container'
-import Footer from './components/Footer'
+import PageBase from './pages/PaginaBase'
 
 function AppRoutes() {
     return (
-<BrowserRouter>
-            <Header />
-            <Container>
-                <Routes>
-                    <Route path="/" element={ <Home /> }></Route>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={ <PageBase /> }>
+                    <Route index element={ <Home /> }></Route>
                     <Route path="/sobre" element={ <Sobre /> }></Route>
                     <Route path="/projetos" element={ <Projetos /> }></Route>
                     <Route path="/contatos" element={ <Contatos /> }></Route>
                     <Route path="*" element={ <Page404 /> }></Route>
-                </Routes>
-            </Container>
-            <Footer />
+                </Route>
+            </Routes>
         </BrowserRouter>
     )
 }
