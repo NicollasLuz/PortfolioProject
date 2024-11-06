297f77af0fd94862b6883e4f44d18b179e14e3d4
Nicollas Luz <nicollasgoias@hotmail.com>
2024-11-06 17:09:12 -0300

    criando página home no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,21 @@ function App() {
   return (
     <>
     <Header />
+    <section className='container'>
+      <div className='apresentacao'>
+        <p>
+          Olá, sou <br/>
+          <span>Rêmulo Borges</span> <br/>
+          Dev Full Stack
+        </p>
+        <button className='btn btn-red'>
+          Saiba mais sobre mim
+        </button>
+      </div>
+      <figure>
+        <img className='img-home' src="/developer-red.svg" alt="Imagem de Home" />
+      </figure>
+    </section>
     <Footer />
     </>
   )
