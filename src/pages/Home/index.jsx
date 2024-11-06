9f6e76ada6dc1024218b54623185bf7224dfe498
Nicollas Luz <nicollasgoias@hotmail.com>
2024-11-06 17:29:34 -0300

    usando o o componente Container

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,28 +1,30 @@
 import { Link } from 'react-router-dom'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
-
-
+import Container from '../../components/Container'
+                
 function Home() {
     return (
        <>
           <Header />
-          <section className='container'>
-            <div className='apresentacao'>
-              <p>
-                Olá, sou <br/>
-                <span>Rêmulo Borges</span> <br/>
-                Dev Full Stack
-              </p>
-              <Link to="/sobre" className='btn btn-red'>
-                    Saiba mais sobre mim
-              </Link>
-            </div>
-            <figure>
-              <img className='img-home' src="/developer-red.svg" alt="Imagem de Home" />
-            </figure>
-          </section>
-          <Footer />
+          <Container>
+            <section className="container">
+                <div className="apresentacao">
+                    <p>
+                        Olá, sou <br />
+                        <span>Rêmulo Borges</span> <br />
+                        Dev Full Stack
+                    </p>
+                    <Link to="/sobre" className="btn btn-red">
+                        Saiba mais sobre mim
+                    </Link>
+                </div>
+                <figure>
+                    <img className="img-home" src="/developer-red.svg" alt="Imagem de Home" />
+                </figure>
+            </section>
+            </Container>
+        <Footer />
         </> 
     )
 }
